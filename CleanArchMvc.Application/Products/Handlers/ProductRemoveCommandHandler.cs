f163a6342cf23bf9c7eec5224f7eb48853ae15bb
Lucas Cardoso de Medeiros <lucascarmed@gmail.com>
2022-08-25 15:10:24 -0300

    CleanArchMvc.Application

---
+++ CleanArchMvc.Application/Products/Handlers/ProductRemoveCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchMvc.Application.Products.Commands;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using MediatR;
+
+namespace CleanArchMvc.Application.Products.Handlers;
+
+public class ProductRemoveCommandHandler : IRequestHandler<ProductRemoveCommand, Product>
+{
+    private readonly IProductRepository _productRepository;
+
+    public ProductRemoveCommandHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository ??
+            throw new ArgumentNullException(nameof(productRepository));
+    }
+
+    public async Task<Product> Handle(ProductRemoveCommand request, CancellationToken cancellationToken)
+    {
+        var product = await _productRepository.GetByIdAsync(request.Id);
+        if (product == null)
+            throw new ApplicationException($"Error removing entity");
+        else
+            return await _productRepository.RemoveAsync(product);
+    }
+}
