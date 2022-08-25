f37544e56c558b72d113db81edf9fc2fa1bd3a61
Lucas Cardoso de Medeiros <lucascarmed@gmail.com>
2022-08-25 15:12:10 -0300

    CleanArchMvc.Infra.Data

---
+++ CleanArchMvc.Infra.Data/Identity/SeedUserRoleInitial.cs
@@ -0,0 +1,70 @@
+﻿using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Identity;
+
+namespace CleanArchMvc.Infra.Data.Identity;
+
+public class SeedUserRoleInitial : ISeedUserRoleInitial
+{
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public SeedUserRoleInitial(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+    {
+        _roleManager = roleManager;
+        _userManager = userManager;
+    }
+
+    public void SeedUsers()
+    {
+        if (_userManager.FindByEmailAsync("usuario@localhost").Result == null)
+        {
+            ApplicationUser user = new ApplicationUser();
+            user.UserName = "usuario@localhost";
+            user.Email = "usuario@localhost";
+            user.NormalizedUserName = "USUARIO@LOCALHOST";
+            user.NormalizedEmail = "USUARIO@LOCALHOST";
+            user.EmailConfirmed = true;
+            user.LockoutEnabled = false;
+            user.SecurityStamp = Guid.NewGuid().ToString();
+
+            IdentityResult result = _userManager.CreateAsync(user, "User@123456").Result;
+            if (result.Succeeded)
+                _userManager.AddToRoleAsync(user, "User").Wait();
+        }
+
+        if (_userManager.FindByEmailAsync("admin@localhost").Result == null)
+        {
+            ApplicationUser user = new ApplicationUser();
+            user.UserName = "admin@localhost";
+            user.Email = "admin@localhost";
+            user.NormalizedUserName = "ADMIN@LOCALHOST";
+            user.NormalizedEmail = "ADMIN@LOCALHOST";
+            user.EmailConfirmed = true;
+            user.LockoutEnabled = false;
+            user.SecurityStamp = Guid.NewGuid().ToString();
+
+            IdentityResult result = _userManager.CreateAsync(user, "Admin@123456").Result;
+            if (result.Succeeded)
+                _userManager.AddToRoleAsync(user, "Admin").Wait();
+        }
+    }
+
+    public void SeedRoles()
+    {
+        if (!_roleManager.RoleExistsAsync("User").Result)
+        {
+            IdentityRole role = new IdentityRole();
+            role.Name = "User";
+            role.NormalizedName = "USER";
+            _ = _roleManager.CreateAsync(role).Result;
+        }
+
+        if (!_roleManager.RoleExistsAsync("Admin").Result)
+        {
+            IdentityRole role = new IdentityRole();
+            role.Name = "Admin";
+            role.NormalizedName = "ADMIN";
+            _ = _roleManager.CreateAsync(role).Result;
+        }
+    }
+}
---
+++ CleanArchMvc.Infra.Data/Migrations/20220726140806_SeedProducts2.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchMvc.Infra.Data.Migrations;
+
+public partial class SeedProducts2 : Migration
+{
+    protected override void Up(MigrationBuilder mb)
+    {
+        mb.Sql(
+            "INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId) " +
+            "VALUES('Caderno espiral', 'Caderno espiral 100 folhas', 7.45, 50, 'caderno1.png', 1)"
+        );
+
+        mb.Sql(
+            "INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId) " +
+            "VALUES('Estojo escolar', 'Estojo escolar cinza', 5.65, 70, 'estojo1.png', 1)"
+        );
+
+        mb.Sql(
+            "INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId) " +
+            "VALUES('Borracha', 'Borracha escolar pequena', 3.25, 80, 'borracha1.png', 1)"
+        );
+
+        mb.Sql(
+            "INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId) " +
+            "VALUES('Calculadora', 'Calculadora escolar simples', 15.39, 20, 'calculadora1.png', 2)"
+        );
+    }
+
+    protected override void Down(MigrationBuilder mb)
+    {
+        mb.Sql("DELETE FROM Products");
+    }
+}
